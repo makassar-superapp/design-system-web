100405d09c8d37014cec2f6590aff04510278645
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2025-12-02 15:26:43 +0700

    feat: integration tables filter and sorting

--- app/components/ui/table/TableHeader.vue
+++ app/components/ui/table/TableHeader.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-type Props = { class?: string }
-const props = defineProps<Props>()
+import { cn } from "@/lib/utils";
+type Props = { class?: string };
+const props = defineProps<Props>();
 </script>
 
 <template>
-  <thead :class="cn('bg-gray-100', props.class)"><slot /></thead>
-  
+  <thead :class="cn('bg-gray-base-0', props.class)">
+    <slot />
+  </thead>
 </template>
--- app/components/ui/table/TableRow.vue
+++ app/components/ui/table/TableRow.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-type Props = { class?: string }
-const props = defineProps<Props>()
+import { cn } from "@/lib/utils";
+type Props = { class?: string };
+const props = defineProps<Props>();
 </script>
 
 <template>
-  <tr :class="cn('hover:bg-gray-50', props.class)"><slot /></tr>
+  <tr :class="cn('hover:bg-gray-100', props.class)">
+    <slot />
+  </tr>
 </template>
