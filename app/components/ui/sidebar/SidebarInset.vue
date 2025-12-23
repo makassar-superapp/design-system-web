589e1b12cad6b2a0b0ee5801d7103b9577d27a9e
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2025-12-23 19:19:06 +0700

    feat: add component ui sidebar

---
+++ app/components/ui/sidebar/SidebarInset.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <main
+    data-slot="sidebar-inset"
+    :class="
+      cn(
+        'bg-white relative flex w-full flex-1 flex-col shadow-xl/30',
+        'md:peer-data-[variant=inset]:m-4 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2',
+        props.class
+      )
+    "
+  >
+    <slot />
+  </main>
+</template>
